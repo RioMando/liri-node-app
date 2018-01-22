c1fd462c5c11ac72425849c4e7ae049ab5e8909e
Armando <roblesrioarmand@gmail.com>
2018-01-21 22:59:03 -0600

    definition of initial variables and set of keys

--- liri.js
+++ liri.js
@@ -1,11 +1,14 @@
+//To red and set environment variables with dotenv package
 require("dotenv").config();
 
-
+//Import keys.js file with Spotify and Tweeter's keys
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+var Twitter = require('twitter');
+
+//Variables to store the keys 
 var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 console.log(spotify);
-
 
