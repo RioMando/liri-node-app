ebd2a61b2439595e559e720e9732052991dc3d69
Armando <roblesrioarmand@gmail.com>
2018-01-26 02:33:35 -0600

    Final program, all sections working properly

--- liri.js
+++ liri.js
@@ -9,17 +9,8 @@ var Spotify = require('node-spotify-api');
 //Import keys.js file with Spotify and Tweeter's keys
 var keys = require("./keys.js");
 
-// Variables to store the keys
-// console.log(keys.spotify);
-// console.log(keys.twitter);
-// console.log(keys.spotify.id);
-// console.log(keys.spotify.secret);
-
 var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
-
-// console.log("Here: " + process.env.TWITTER_CONSUMER_KEY) 
-
 
 // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
 var request = require("request");
@@ -38,92 +29,62 @@ var option = process.argv[2];
 var nameMovieOrSong = process.argv[3];
 var valid = false;
 
-switch (option) {
-	case "my-tweets":
-	valid = true;	
-	myTweets();
-	break;
+options(option);
 
-	case "spotify-this-song":
-	valid = true;
-	if (!nameMovieOrSong) {
-		nameMovieOrSong = "The Sign";
+function options (option) {
+	switch (option) {
+		case "my-tweets":
+		valid = true;	
+		myTweets();
+		break;
+
+		case "spotify-this-song":
+		valid = true;
+		if (!nameMovieOrSong) {
+			nameMovieOrSong = "The Sign";
+		}
+		console.log("\nLooking for \'" + nameMovieOrSong + "\' song.");
+		spotifyThis();
+		break;
+
+		case "movie-this":
+		valid = true;
+		if (!nameMovieOrSong) {
+			nameMovieOrSong = "Mr. Nobody.";
+		}
+		movie();
+		break
+
+		case "do-what-it-says":
+		valid = true;
+		doWhatItSays();
+		break
 	}
-	console.log("\nLooking for \"" + nameMovieOrSong + "\" song.");
-	spotifyThis();
-	break;
 
-	case "movie-this":
-	valid = true;
-	if (!nameMovieOrSong) {
-		nameMovieOrSong = "Mr. Nobody.";
+	//In case that the given option is not in the above 4
+	if (!valid){
+		console.log("\n" + option + " Is not a valid option. Try Again!\n\nValid options are:");
+		console.log("  my-tweets\n  spotify-this-song\n  movie-this\n  do-what-it-says");
 	}
-	movie();
-	break
-
-	case "do-what-it-says":
-	valid = true;
-	doWhatItSays();
-	break
 }
 
-//In case that the given option is not in the above 4
-if (!valid){
-	console.log("\n" + option + " Is not a valid option. Try Again!\n\nValid options are:");
-	console.log("  my-tweets\n  spotify-this-song\n  movie-this\n  do-what-it-says");
-}
-
-// 
+ 
 function myTweets() {
 	var params = {screen_name: '', count:20, user_id: 955267080353468416};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {	
 	    if (!error) {
 	    	tweetResponse = tweets;
-	    	counterTw = 0;
-	    	
-	    	timeArry = [];
-	    	displayTweets();
-	    	// console.log(tweets);
-		}//End of if(!error)
+	    	counterTw = tweetResponse.length-1;
+
+			for (counterTw; counterTw >= 0; counterTw--) {
+				var time = tweetResponse[counterTw].created_at;	
+				var timeDlls = moment.utc(time, 'ddd MMM DD HH:mm:ss Z <YYYY></YYYY>').tz('America/Chicago').format("lll");
+				console.log("\n" + timeDlls);
+				console.log(tweetResponse[counterTw].text);
+			}//End of if(!error)
+		}
 	});// End of client.get
-
 } //End of myTweets
-
-function displayTweets() {
-   	if (counterTw < tweetResponse.length) {
-		console.log("\n\n" + tweetResponse[counterTw].text + "\n" +  tweetResponse[counterTw].created_at);
-		var time = tweetResponse[counterTw].created_at;	
-		console.log(tweetResponse[0]);
-	 var timeArry = time.split(" ");
-	 console.log("Array splice: ", timeArry);
-	 timeArry.splice(3, 0, timeArry[5]);
-	 timeArry.splice(5, 2);
-	 timeArry.splice(0, 1);
-	 console.log(timeArry);
-	 var displayTime = "";
-	 // for (var j=0; j<4; j++) {
-		 // displayTime = displayTime + timeArry[j] + " ";
-		 
-	// }
-	displayTime = timeArry[0] + " "+timeArry[1] + " "+timeArry[2] + " "+timeArry[3];
-	console.log(displayTime);
-	
-	var timeCT = moment.utc(displayTime, 'MMM DD YYYY HH:mm:ss').tz('America/Chicago').format("YYYY-MM-DD HH:mm");
-	console.log("Time comverted:" +timeCT);
-
-	// var newYork    = moment.tz(displayTime, "America/New_York");
-	// var losAngeles = newYork.clone().tz("America/Los_Angeles");
-	// var london     = newYork.clone().tz("Europe/London");
-	// newYork.format();
-	// losAngeles.format();
-	// london.format();
-
-	counterTw++;
-	// displayTweets();
-	} //End if-loop
-	
-} //End of displayTweets 
-
 
 function spotifyThis() {
 
@@ -192,6 +153,17 @@ function movie() {
 }
 
 function doWhatItSays() {
-	console.log("Here goes theJSON.parse(body).Year code for Do Wat It Says");
-};
+	fs.readFile("random.txt", "utf8", function(err, data) {
+	    if (err) {
+	      return console.log(err);
+	    }
+	    // Break down the information from random.txt usiing comma ","
+	    doWhat = data.split(",");
+	    option = doWhat[0];
+		nameMovieOrSong = doWhat[1];
+		valid = false;
+		//calling function option
+		options (option);
+  	});
+}
 
